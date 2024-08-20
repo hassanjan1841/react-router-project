997048afca946c7a985d1db25cc813cf941e9462
Hassan Jan <161922826+hassanjan1841@users.noreply.github.com>
2024-08-20 19:52:34 +0500

    the all navigation pages setup

--- src/main.jsx
+++ src/main.jsx
@@ -10,11 +10,15 @@ import {
 } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact-us" element={<Contact />} />
     </Route>
   )
 );
